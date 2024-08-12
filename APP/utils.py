76f1281737224c7733b0be5c5146568952482be0
Kristian Alikaj <92834350+kris96tian@users.noreply.github.com>
2024-08-12 12:55:49 +0200

    Update utils.py

--- APP/utils.py
+++ APP/utils.py
@@ -10,9 +10,8 @@ def load_hdf5(file_path, dataset_name):
     with h5py.File(file_path, 'r') as f:
         ecg_data = f[dataset_name][:]
     return ecg_data.T  # -> to (leads, time_points)
-
+    
 def preprocess_ecg_data(ecg_data, target_length=5300):
-    #data must have 12 leads and a spec. number of time points
     num_leads, num_points = ecg_data.shape
     
     if num_leads != 12:
@@ -24,5 +23,20 @@ def preprocess_ecg_data(ecg_data, target_length=5300):
     elif num_points < target_length:
         padding = target_length - num_points
         ecg_data = np.pad(ecg_data, ((0, 0), (0, padding)), 'constant')  
+    
+    return ecg_data
+
+
+    
+def load_data(uploaded_file, file_type, dataset_name, target_length):
+    file_path = uploaded_file
+        if file_type == 'csv':
+        ecg_data = load_csv(file_path)
+    elif file_type == 'hdf5':
+        ecg_data = load_hdf5(file_path, dataset_name)
+    else:
+        raise ValueError("Unsupported file type. Please upload a CSV or HDF5 file.")
+    
+    ecg_data = preprocess_ecg_data(ecg_data, target_length)
     
     return ecg_data
